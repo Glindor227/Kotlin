5971eeea0951bfbe1cc07baef98829e49e408513
Андрей <glindor227@gmail.com>
2020-02-23 14:56:11 +0300

    Исправил незамеченную ошибку с курсором

--- app/src/main/java/com/geekbrains/kotlin/ui/note/NoteActivity.kt
+++ app/src/main/java/com/geekbrains/kotlin/ui/note/NoteActivity.kt
@@ -72,8 +72,8 @@ class NoteActivity : BaseActivity<NoteViewState.Data, NoteViewState>() {
     private fun initView() {
         note?.let { note ->
             removeEditListener()
-            et_title.setText(note.title)
-            et_body.setText(note.text)
+            if(et_title.text.toString()!=note.title) et_title.setText(note.title)
+            if(et_body.text.toString()!=note.text) et_body.setText(note.text)
             color = note.bgrColor
             toolbar.setBackgroundColor(color.getColorInt(this))
             supportActionBar?.title = SimpleDateFormat(DATE_TIME_FORMAT, Locale.getDefault()).format(note.lastChanged)
